7513ae3169e94cd133791dcab9a475a713cc9112
Estiven Mayhuay <developerwebhairton@gmail.com>
2023-11-29 21:30:00 -0500

    add image public

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    assetsDir: 'images'
-  },
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
 })
