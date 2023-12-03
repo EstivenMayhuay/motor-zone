c69f97d80d78bf09f83d930217de120fbd97e6d9
Estiven Mayhuay <developerwebhairton@gmail.com>
2023-12-02 20:57:43 -0500

    change view servicios

--- src/components/NuestrosServicios/index.jsx
+++ src/components/NuestrosServicios/index.jsx
@@ -1,3 +1,4 @@
+import { Card } from 'react-bootstrap'
 import './index.css'
 
 export const NuestrosServicios = ({
@@ -8,18 +9,20 @@ export const NuestrosServicios = ({
         <div id='servicios' className='bg-primary-motor px-4'>
             <h6 className='title text-center fw-bold text-white'>Nuestros Servicios</h6>
 
-            <div className='align-items-center justify-content-center servicios__container'>
+            <div className='servicios__container'>
             {
                 servicios &&
                 // eslint-disable-next-line react/prop-types
                 servicios.map((servicio, iter) => {
                     return <>
-                        <ul key={iter}className='servicios__list m-0 p-0'>
-                            <li className='text-white'>
-                                <span className='fw-bold'>-</span>
-                                <p className='text-white paragraph'>{servicio.name}</p>
-                            </li>
-                        </ul>
+                        <Card className='bg-second-motor border-0 servicioCard' key={iter}>
+                            <Card.Body className='position-relative servicioCard__body'>
+                                <p className='text-white paragraph m-0 px-2 text-break'>{servicio.name}</p>
+                                <div className='text-right'>
+                                    <img src="./images/icon-car.svg" alt="Motor Zone" width={32} height={32} className='' />
+                                </div>
+                            </Card.Body>
+                        </Card>
                     </>
                 })
             }
@@ -28,3 +31,9 @@ export const NuestrosServicios = ({
     )
 }
 
+                {/* <ul key={iter}className='servicios__list m-0 p-0'>
+                    <li className='text-white'>
+                        <span className='fw-bold'>-</span>
+                        <p className='text-white paragraph'>{servicio.name}</p>
+                    </li>
+                </ul> */}
