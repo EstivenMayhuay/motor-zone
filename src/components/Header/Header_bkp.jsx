30cbe46db2a4eb923f9a000696cb4854621a9c59
Estiven Mayhuay <developerwebhairton@gmail.com>
2024-02-17 23:56:37 -0500

    agregando nuevos datos

---
+++ src/components/Header/Header_bkp.jsx
@@ -0,0 +1,221 @@
+import { Button, Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import './index.css';
+import { NavLink, useLocation } from 'react-router-dom';
+import { Fade, Slide } from 'react-awesome-reveal'
+import { faCalendar, faMapLocation, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import { ButtonSocial } from '../ButtonSocial/ButtonSocial';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const Header = ({
+    headerTitle = '',
+    headerImgSrc = '',
+    headerText = ''
+}) => {
+    const [title, setTitle] = useState(headerTitle);
+    const [delayAnimate, setDelayAnimate] = useState(400);
+    const [imgSrc, setImgSrc] = useState(headerImgSrc);
+    const [showMenuMobile, setShowMenuMobile] = useState(false);
+    const handleShowCanvas = () => setShowMenuMobile(true);
+    const location = useLocation();
+    const [index, setIndex] = useState(0);
+    const arrImages = ['bg-fachada.png', 'bg-fachada-2.png', 'bg-fachada-3.png'];
+    const arrText = ['Excelente Atención para su Vehículo', 'Calidad y Confianza Garantía de Servicio', 'Óptimo Servicio con Técnicos Calificados'];
+    const arrInfo = [
+        <>
+            <ul className="text-left" style={{listStyle: "none"}}>
+                <li>Mecanica en General</li>
+                <li>Mantenimiento</li>
+                <li>Preventivo / Correctivo</li>
+                <li>Alineamiento De Direccion</li>
+                <li>Planchado y Pintura</li>
+            </ul>
+        </>
+    ];
+
+    const NUM_IMAGES = arrImages.length - 1;
+    const [timeInterval, setTimeInterval]  = useState(0);
+
+    const handleCarousel = (e, img, text) => {
+        // setImgSrc(img);
+        setTitle(text);
+
+        setTimeInterval(0);
+        clearInterval(timeInterval)
+
+        let activeBtn = document.querySelectorAll('.btnCircle.active');
+
+        let header = document.querySelector(".header");
+        header.style.backgroundImage = `linear-gradient(180deg, rgba(0,0,0,0.25) 0%, rgba(0,0,0,0.5) 74%, rgba(0,0,0,1) 100%),
+        url('../images/${img}')`;
+
+        activeBtn.forEach((btn) => btn.classList.remove('active'))
+
+        if(e.target.classList.contains('active')) {
+            e.target.classList.remove('active')
+        } else {
+            e.target.classList.add('active')
+        }
+    }
+
+    const carouselHome = () => {
+        let header = document.querySelector('.header');
+        let btnCircles = document.querySelectorAll('.btnCircle');
+        let activeBtn = document.querySelectorAll('.btnCircle.active');
+        
+        let timer = setTimeout(() => {
+            index === NUM_IMAGES ? setIndex(0) : setIndex(index + 1);
+        }, 6000)
+
+        // setImgSrc(arrImages[index]);
+        setTitle(arrText[index]);
+
+        activeBtn.forEach((btn) => btn.classList.remove('active'));
+        
+        header.style.backgroundImage = `linear-gradient(180deg, rgba(0,0,0,0.25) 0%, rgba(0,0,0,0.5) 74%, rgba(0,0,0,1) 100%),
+        url('../images/${arrImages[index]}')`;
+        btnCircles[index].classList.add('active');
+        
+        setTimeInterval(timer);
+    }
+
+    useEffect(() => {
+        if(location.pathname === '/') {
+            carouselHome();
+        }
+        else {
+            let header = document.querySelector(".header");
+            header.style.backgroundImage = `linear-gradient(180deg, rgba(0,0,0,0.25) 0%, rgba(0,0,0,0.5) 74%, rgba(0,0,0,1) 100%),
+            url('../images/${imgSrc}')`;
+        }
+
+    }, [imgSrc, index])
+
+    return (
+        <header className="header position-relative">
+            <div className="header__top px-4 d-flex align-items-center mb-3">
+                <div className="header__logo">
+                    <a href="/">
+                        <img src="./images/logo-main.png" alt="" width={180} />
+                    </a>
+                </div>
+
+                <div className="header__social w-100 d-flex justify-content-end">
+                    <ul className="list-inline m-0 p-0 d-flex align-items-center gap-4">
+                        <li>
+                            <ButtonSocial type={'facebook'} />
+                        </li>
+                        <li>
+                            <ButtonSocial type={'instagram'} />
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div className="header__button">
+                    <button onClick={handleShowCanvas} className='m-0 p-0 border-0 bg-transparent'>
+                        <img src="./images/icon-hamburguer-right.svg" alt="Motor Zone" width={36} />
+                    </button>
+                </div>
+
+                <Offcanvas show={showMenuMobile} onHide={() => setShowMenuMobile(false)} className='bg-primary-motor' placement='end'>
+                    <Offcanvas.Header>
+                        <Offcanvas.Title className='text-white fw-bold'>Motor Zone</Offcanvas.Title>
+                        <Button className='bg-transparent p-0 m-0 border-0' onClick={() => setShowMenuMobile(false)}>
+                            <img src="./images/icon-close.svg" alt="Motor Zone" width={36} />
+                        </Button>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body className='d-flex flex-column gap-3'>
+                        <ul className='navBarMobile'>
+                            <NavLink to={'/'} activeclassname='active' className='header__link--mobile'>¿Quienes Somos?</NavLink>
+                            <NavLink to={'/promociones'} activeclassname='active' className='header__link--mobile'>Promociones</NavLink>
+                            <NavLink to={'/nuestros-clientes'} activeclassname='active' className='header__link--mobile'>Nuestros Clientes</NavLink>
+                            <NavLink to={'/nuestros-servicios'} activeclassname='active' className='header__link--mobile'>Nuestros Servicios</NavLink>
+                            <NavLink to={'/linea-de-repuestos'} activeclassname='active' className='header__link--mobile'>Linea de Repuestos</NavLink>
+                            <NavLink to={'/contactanos'} activeclassname='active' className='header__link--mobile'>Contactanos</NavLink>
+                        </ul>
+                        <div className="navBarMobile__social d-flex gap-4" style={{marginLeft: '1.75rem'}}>
+                            <ButtonSocial type={'facebook'} />
+                            <ButtonSocial type={'instagram'} />
+                        </div>
+                    </Offcanvas.Body>
+                </Offcanvas>
+            </div>
+
+            <div className="header__navbar px-4">
+                <Navbar id='navbar__main' className='bg-primary-motor w-100 rounded'>
+                    <Container>
+                        <Nav className='m-auto header__nav'>
+                            <NavLink to={'/'} activeclassname='active' className='header__link'>
+                                ¿Quiénes Somos?
+                            </NavLink>
+                            <NavLink to={'/promociones'} activeclassname='active' className='header__link'>
+                                Promociones
+                            </NavLink>
+                            <NavLink to={'/nuestros-clientes'} activeclassname='active' className='header__link'>
+                                Nuestros Clientes
+                            </NavLink>
+                            <NavLink to={'/nuestros-servicios'} activeclassname='active' className='header__link'>
+                                Nuestros Servicios
+                            </NavLink>
+                            <NavLink to={'/linea-de-repuestos'} activeclassname='active' className='header__link'>
+                                Linea de Repuestos
+                            </NavLink>
+                            <NavLink to={'/contactanos'} activeclassname='active' className='header__link'>
+                                Contáctenos
+                            </NavLink>
+                        </Nav>
+                    </Container>
+                </Navbar>
+            </div>
+
+            <Slide delay={200}>
+                <div className="header__title px-5">
+                    <Slide delay={delayAnimate} direction='ltr'>
+                        <h1 className='mb-5' dangerouslySetInnerHTML={{__html: title}}></h1>
+                    </Slide>
+                    {
+                        title === "Excelente Atención para su Vehículo" &&
+                        <Fade cascade delay={delayAnimate}>
+                            <ul className="d-block text-left" style={{listStyle: "none"}}>
+                                <li>Mecanica en General</li>
+                                <li>Mantenimiento</li>
+                                <li>Preventivo / Correctivo</li>
+                                <li>Alineamiento De Direccion</li>
+                                <li>Planchado y Pintura</li>
+                            </ul>
+                        </Fade>
+                    }
+                    {
+                        location.pathname === "/contactanos" &&
+                        <div className="paragraph text-white d-flex flex-column gap-2">
+                            <span className="d-flex gap-3">
+                                <FontAwesomeIcon icon={faMapLocation} size="lg" className="color-second-motor" />
+                                AV. PASEO DE LA REPÚBLICA 1156, URB SANTA BEATRIZ 
+                            </span>
+
+                            <span className="d-flex gap-3">
+                                <FontAwesomeIcon icon={faPhone} size="lg" className="color-second-motor" />
+                                920288419 / 993162796 / 999183000
+                            </span>
+
+                            <span className="d-flex gap-3">
+                                <FontAwesomeIcon icon={faCalendar} size="lg" className="color-second-motor" />
+                                Lun a Vie 8:00am a 6pm Sáb: 8am a 1pm
+                            </span>
+                        </div>
+                    }
+                </div>
+            </Slide>
+
+            {
+                location.pathname === '/' &&
+                <div className='header__buttons position-absolute w-100 bottom-0 d-flex justify-content-center align-items-center p-2 gap-2' style={{marginBottom: '2rem'}}>
+                    <button className='btnCircle active' onClick={(e) => handleCarousel(e, 'bg-fachada.png', 'Excelente Atención para su Vehículo')}></button>
+                    <button className='btnCircle' onClick={(e) => handleCarousel(e, 'bg-fachada-2.png', 'Calidad y Confianza Garantía de Servicio')}></button>
+                    <button className='btnCircle' onClick={(e) => handleCarousel(e, 'bg-fachada-3.png', 'Óptimo Servicio con Técnicos Calificados')}></button>
+                </div>
+            }
+        </header>
+    )
+}
